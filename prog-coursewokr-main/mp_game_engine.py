5595c4895a86a77198260395e4b8c0b583d4dce6
Negativity7477 <finngosney1@hotmail.com>
2023-12-02 15:32:23 +0000

    2nd dec

--- prog-coursewokr-main/mp_game_engine.py
+++ prog-coursewokr-main/mp_game_engine.py
@@ -15,12 +15,12 @@ def initialise_players(player:str = "two") -> dict:
     """
     
     global username_1
-    username_1 = input("Enter username one")
-    board = components.initialise_board()
-    battleships_1 = components.create_battleships()
+    username_1 = input("Enter username one") #Globalise the username to use elsewhere
+    board = components.initialise_board() 
+    battleships_1 = components.create_battleships() 
     board_1 = components.place_battleships(board=board, ships = battleships_1, algorithm= "custom")
     dict_values_1 = [board_1, battleships_1]
-    if player == "two":
+    if player == "two": #Can have a second player or AI
         global username_2
         username_2 = input("Enter username two")
     else:
@@ -28,6 +28,7 @@ def initialise_players(player:str = "two") -> dict:
     battleships_2 = components.create_battleships()
     board_2 = components.place_battleships(board=board, ships = battleships_2, algorithm="random")
     dict_values_2 = [board_2, battleships_2]
+    #Create values for the other player, either AI or another person
     
     players = {username_1: dict_values_1, username_2:dict_values_2}
     
@@ -57,19 +58,25 @@ def ai_opponent_game_loop():
     ai_ships = players["AI"][1]
     player_board = players[username_1][0]
     player_ships = players[username_1][1]
-    finished = False
-    won = None
+    finished = False #flag to loop through
     while not finished:
         attack_coords = game_engine.cli_coordinates_input()
-        game_engine.attack(attack_coords, ai_board, ai_ships)
-        game_engine.attack(generate_attack(player_board), player_board, player_ships)
-        no_ai_ships = all(value == 0 for value in ai_ships)
-        no_player_ships = all(value == 0 for value in player_ships)
         
-        if no_ai_ships:
+        print("Player, ")#Allows the ability to distinguish between players and AI attacks
+        player_hit = game_engine.attack(attack_coords, ai_board, ai_ships)
+        
+        print("Ai, ") #Allows the ability to distinguish between players and AI attacks
+        ai_attack = generate_attack(player_board)
+        ai_hit = game_engine.attack(ai_attack, player_board, player_ships)
+
+        no_ai_ships = all(value == 0 for value in ai_ships)  
+        no_player_ships = all(value == 0 for value in player_ships)
+        #Check all ship sizes in the both dictionaries.
+        
+        if no_ai_ships: #All ai ships have been sunk
             print("Congratulations! You have won.")
             finished = True
-        elif no_player_ships:
+        elif no_player_ships: #All player ships haveb een sunk
             print("Unfortunately, you have lost.")
             finished = True
     
